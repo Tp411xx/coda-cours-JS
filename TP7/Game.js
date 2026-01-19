cdbbe51ffc34ed7c4191e6639a4802a3f7ac975a
Thomas <thoma@DESKTOP-M64J6D1>
2026-01-19 13:38:22 +0100

    add exemples

--- TP7/Game.js
+++ TP7/Game.js
@@ -7,7 +7,7 @@ const backendData = {
     "3cd71bbb-6a6b-4d4e-80e3-107130328a27": {
       name: "blabla",
       skinPath: "./spritesheets/3.png",
-      position: [0.5600000000000003, 0.17999999999999977],
+      position: [0, 0],
       lvl: 1,
       hp: 100,
       maxHp: 100,
@@ -40,10 +40,10 @@ const backendData = {
 };
 
 class Game {
-  constructor(backendData) {
-    this.isRunning = backendData.isRunning;
-    this.isOver = backendData.isOver;
-    this.timer = backendData.timer;
+  constructor() {
+    this.isRunning = false;
+    this.isOver = false;
+    this.timer = 0;
 
     this.players = {};
   }
@@ -68,7 +68,7 @@ class Game {
           playerId,
           playerData.name,
           playerData.skinPath,
-          playerData.position
+          playerData.position,
         );
       }
 
@@ -85,3 +85,47 @@ class Game {
     }
   }
 }
+
+console.log("==========Instance de la Game 1==========");
+
+const game1 = new Game();
+game1.update(backendData);
+console.log(structuredClone(game1));
+
+console.log("==========MetaData Test==========");
+backendData.isOver = true;
+backendData.timer = 192;
+backendData.players["28ead291-fcea-4b41-a596-d3c876c49a53"].name =
+  "MetaDataTEST";
+backendData.players["3cd71bbb-6a6b-4d4e-80e3-107130328a27"].hp = 2;
+game1.update(backendData);
+console.log(structuredClone(game1));
+
+console.log("==========AddPlayer Test==========");
+const newPlayer = "new-player-666";
+
+backendData.players[newPlayer] = {
+  name: "Le Petit Nouveau",
+  skinPath: "./assets/1.png",
+  position: [0.5, 0.5],
+  lvl: 1,
+  hp: 100,
+  maxHp: 100,
+  hpRegenRate: 10,
+  speed: 0.2,
+  direction: 1,
+  isAttacking: false,
+  isWalking: true,
+  isDying: false,
+  attackCooldown: 1,
+  currentAttackCooldown: 0,
+};
+
+game1.update(backendData);
+console.log(structuredClone(game1));
+
+console.log("==========DeletePlayer Test==========");
+
+delete backendData.players[newPlayer];
+game1.update(backendData);
+console.log(structuredClone(game1));
